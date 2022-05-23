4bb3f6b7031edfeeafe7d65c2b82359d7dc0e73b
Justyna Łuczak <justyna.macniak@gmail.com>
2022-05-23 12:32:41 +0200

    BookListView from generic.ListView; urls for BOOKS APP, template HTML for book_list

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    template = "book_list.html"
--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books.apps.BooksConfig',
-    'rest_framework',
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
