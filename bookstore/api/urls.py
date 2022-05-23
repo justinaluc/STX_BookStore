769fc8ca122dee8b6f6de4932866a4e2f45d431f
Justyna Łuczak <justyna.macniak@gmail.com>
2022-05-23 22:32:23 +0200

    class based views: BOOKS list /GET/, BOOK: add /POST/, details /GET/, update /PATCH/, delete /DELETE/

--- bookstore/api/urls.py
+++ bookstore/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import get_api_spec, BookApiView
+from .views import get_api_spec, BookList, BookDetail
 
 urlpatterns = [
     path('api_spec/', get_api_spec, name='api_specification'),
-    path('books/', BookApiView.as_view(), name='api_book_list_class'),
+    path('books/', BookList.as_view(), name='book_list'),
+    path('books/<int:pk>/', BookDetail.as_view(), name='book_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- bookstore/api/views.py
+++ bookstore/api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics
+from django.http import Http404
+
+from rest_framework import status, generics
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -6,16 +9,59 @@ from books.models import Book
 from .serializers import BookSerializer
 
 
-class BookApiView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
 @api_view(['GET'])
 def get_api_spec(request):
+    """gives a rigid specification data about required api project version"""
     spec = {
         "info": {
             "version": "2022.05.16"
             }
         }
     return Response(spec)
+
+
+class BookList(APIView):
+    """
+    one class-based view for listing all book instances or create new book.
+    """
+    def get(self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookDetail(APIView):
+    """
+    view for get, update/patch or delete a book instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        book = self.get_object(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
